a392f305cbce82201c2841ebd58c03ebe119c6c4
avive <aviveyal07@gmail.com>
2023-06-04 22:10:50 +0300

    Update to use josn body args

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # cloud-functions
 Karma Coin Firebase Cloud functions 
+
+
+
+## Testing 
+
+### Running on production
+
+```bash
+curl -m 70 -X POST https://processpaymenttransaction-4325ps3n6q-uc.a.run.app \
+-H "Authorization: bearer $(gcloud auth print-identity-token)" \
+-H "Content-Type: application/json" \
+-d '{
+  "toId": "v7nnqiFVAXBP/ZhFKji8kNxirSOMnVtjShFqs+a6+/Y=",
+  "amount": "1 Karma Coin",
+  "txId": "0x12345"
+}'
+```
+
+### Running with local emulator
+
+Obtain the functions url from the emulator web ui.
+
+```bash
+curl -m 70 -X POST http://127.0.0.1:5001/karmacoin-83d45/us-central1/processPaymentTransaction \
+-H "Authorization: bearer $(gcloud auth print-identity-token)" \
+-H "Content-Type: application/json" \
+-d '{
+  "toId": "v7nnqiFVAXBP/ZhFKji8kNxirSOMnVtjShFqs+a6+/Y=",
+  "amount": "1 Karma Coin",
+  "txId": "0x123012"
+}'
+```
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,9 @@
-import {onRequest} from "firebase-functions/v2/https";
+import {onRequest, HttpsError} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import admin = require("firebase-admin");
 import {getFirestore} from "firebase-admin/firestore";
+// import {messaging} from "firebase-admin";
+// import BatchResponse = messaging.BatchResponse;
 
 // init firebase admin
 admin.initializeApp();
@@ -10,28 +12,28 @@ const db = getFirestore();
 // Send push note to Karma Coin Transaction receiver account
 export const processPaymentTransaction =
     onRequest(async (request, response) => {
-      const toId = request.query.toId?.toString();
-      if (toId === null) {
+      const toId : string = request.body.toId || "";
+      if (toId === "") {
         logger.log("missing required toId parameter");
-        return;
+        throw new HttpsError("invalid-argument", "missing required toId param");
       }
-      const amount = request.query.amount?.toString();
+      const amount = request.body.amount?.toString();
       if (amount === null) {
         logger.log("missing required amount parameter");
-        return;
+        throw new HttpsError("invalid-argument", "missing amount param");
       }
 
-      const charTraitId = request.query.charTrait?.toString() || "";
-      const emoji = request.query.emoji?.toString() || "🎉";
-      const transactionId = request.query.transactionId?.toString();
+      const charTraitId = request.body.charTrait?.toString() || "";
+      const emoji = request.body.emoji?.toString() || "🎉";
+      const transactionId = request.body.txId?.toString();
 
       if (transactionId === null) {
         logger.log("missing required transaction id parameter");
-        return;
+        throw new HttpsError("invalid-argument", "missing transactionId param");
       }
 
-      const accountId = Buffer.from(toId!, "base64").toString("ascii");
-      logger.log("Payment tx to accountId: " + accountId);
+      // const accountId = Buffer.from(toId, "base64").toString("utf-8");
+      // logger.log("Payment tx to accountId: " + accountId);
 
       // Create a reference to the users collection
       const usersRef = db.collection("users");
@@ -41,6 +43,7 @@ export const processPaymentTransaction =
 
       if (docs.empty) {
         logger.log("No push notes token for account.");
+        response.status(200).send("no push notes for account");
         return;
       }
 
@@ -49,11 +52,11 @@ export const processPaymentTransaction =
       // collect all push tokens into pushTokens
       // note multiple tokens per doc possible
       docs.forEach((doc) => {
-        const tokens : Array<Map<string, string>> = doc.get("tokens");
-        tokens.forEach((tokenData) => {
-          const val = tokenData.get("token");
-          if (val != null) {
-            pushTokens.push(val);
+        const tokens: Array<any> = doc.get("tokens");
+        tokens.forEach(function(tokenData) {
+          const token = tokenData.token;
+          if (token !== null) {
+            pushTokens.push(token);
           }
         });
       });
@@ -64,12 +67,26 @@ export const processPaymentTransaction =
       } else {
         logger.log("pushTokens found: " + pushTokens.length);
 
-        sendPushNotes(pushTokens, amount!, charTraitId, transactionId!, emoji);
+        sendPushNotes({
+          tokens: pushTokens,
+          amount: amount,
+          charTrait: charTraitId,
+          transactionId: transactionId,
+          emoji: emoji,
+        });
+
+        response.status(200).send("push notes sent");
       }
     });
 
 // eslint-disable-next-line max-len,require-jsdoc
-function sendPushNotes(tokens:Array<string>, amount:string, charTrait: string, transactionId: string, emoji: string) {
+function sendPushNotes({tokens, amount, charTrait, transactionId, emoji}: {
+    tokens: Array<string>,
+    amount: string,
+    charTrait: string,
+    transactionId: string,
+    emoji: string
+}) {
   logger.log("Sending push note to tokens: " + tokens);
 
   // eslint-disable-next-line max-len
@@ -83,15 +100,49 @@ function sendPushNotes(tokens:Array<string>, amount:string, charTrait: string, t
       body: message,
     },
     data: {
-      transactionId: transactionId,
+      txId: transactionId,
       message: message,
     },
   };
 
   admin.messaging().sendEachForMulticast(payload).then((response) => {
     // Response is a message ID string.
-    logger.log("Successfully sent message:", response);
+    logger.log("Successfully sent messages: " + response.successCount +
+        ", failures: " + response.failureCount);
+
+    response.responses.forEach((resp) => {
+      if (!resp.success) {
+        logger.log("response error: " + resp.error);
+      } else {
+        logger.log("message sent: " + resp.messageId);
+      }
+    });
+
+    // todo: process response and delete tokens that are no longer valid
   }).catch((error) => {
     logger.log("Error sending message:", error);
   });
 }
+
+// // Cleans up the tokens that are no longer valid.
+// // function cleanupTokens(response, tokens) {
+//    // For each notification we check if there was an error.
+//    const tokensDelete = [];
+//    response.results.forEach((result, index) => {
+//        const error = result.error;
+//        if (error) {
+// eslint-disable-next-line max-len
+//            functions.logger.error('Failure sending notification to', tokens[index], error);
+//            // Cleanup the tokens that are not registered anymore.
+//            if (error.code === 'messaging/invalid-registration-token' ||
+// eslint-disable-next-line max-len
+//                error.code === 'messaging/registration-token-not-registered') {
+//                // eslint-disable-next-line max-len
+// eslint-disable-next-line max-len
+//                const deleteTask = admin.firestore().collection('fcmTokens').doc(tokens[index]).delete();
+//                tokensDelete.push(deleteTask);
+//            }
+//        }
+//    });
+//    return Promise.all(tokensDelete);
+// }
